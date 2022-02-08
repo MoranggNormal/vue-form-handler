4bcb970684816badf44bbe0d6bc751a4be429f49
Euller Peixoto <peixotoeuller500@gmail.com>
2022-02-07 23:36:10 -0300

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div id="app">
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+
     <Home test="Hello Vue!" />
   </div>
 </template>
+
+
+<script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous">
+</script>
 
 <script>
 import Home from "./components/Home.vue";
