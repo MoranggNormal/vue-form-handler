57dca5c9d3e782911507dd78bf76c5e73b4f89ad
Euller Peixoto <peixotoeuller500@gmail.com>
2022-02-08 08:39:06 -0300

    move bootstrap links to index.html

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <template>
   <div id="app">
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-      crossorigin="anonymous"
-    />
-
     <div class="container my-4 col-lg-6 col-xl-4">
       <div class="d-flex justify-content-center">
         <img
@@ -67,12 +60,6 @@
   </div>
 </template>
 
-
-<script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-      crossorigin="anonymous">
-</script>
 
 <script>
 import Input from "./components/Input.vue";
@@ -177,6 +164,8 @@ export default {
         confirmPassword: getDataFromTemplate[5],
         cpf: getDataFromTemplate[6],
       };
+
+      console.log(user);
     },
   },
 };
